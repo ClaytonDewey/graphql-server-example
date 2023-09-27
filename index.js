2d46cf1ddcd9a05f0cc28a9bc57885b7795cc801
ClaytonDewey <clay@dryadmedia.com>
2023-09-27 17:14:33 -0500

    Start work on mutations

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ const resolvers = {
     },
     /**
      * review(parent
-     * [refers to the parent resolver in the chain], 
-     * args[arguments], 
+     * [refers to the parent resolver in the chain],
+     * args[arguments],
      * context[for supplying context variables]) {
      *   return
      * }
@@ -40,7 +40,7 @@ const resolvers = {
   Game: {
     reviews(parent) {
       return db.reviews.filter((r) => r.game_id === parent.id);
-    }
+    },
   },
   Review: {
     author(parent) {
@@ -48,13 +48,28 @@ const resolvers = {
     },
     game(parent) {
       return db.games.find((g) => g.id === parent.game_id);
-    }
+    },
   },
   Author: {
     reviews(parent) {
-      return db.reviews.filter((r) => r.author_id === parent.id)
-    }
-  }
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((g) => g.id !== args.id);
+
+      return db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      db.games.push(game);
+      return db.games;
+    },
+  },
 };
 
 // Server setup
@@ -72,5 +87,5 @@ console.log(`Server ready at port`, 4000);
 /**
  * Links for course:
  * https://www.apollographql.com/docs/apollo-server/getting-started/
- * https://www.youtube.com/watch?v=5199E50O7SI  46:24 (06 - Query Variables)
+ * https://www.youtube.com/watch?v=5199E50O7SI  1:18:18 (06 - Query Variables)
  */
