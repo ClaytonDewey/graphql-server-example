2c37124b6e36a5e7e70caa977aff9eb3cabd63f0
ClaytonDewey <clay@dryadmedia.com>
2023-09-27 13:26:25 -0500

    Import dummy data and create resolvers

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// data
+import db from './db.js';
+
 // types
 import { typeDefs } from './schema.js';
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    reviews(_, args) {
+      return db.reviews.find((reviews) => reviews.id === args.id);
+    },
+  },
+};
 
 // Server setup
 const server = new ApolloServer({
   typeDefs,
-  // resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
