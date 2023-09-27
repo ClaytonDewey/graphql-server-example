34038c9f67d2b1f91ad8ec3275ecbd16f5d643ac
ClaytonDewey <clay@dryadmedia.com>
2023-09-27 13:19:42 -0500

    Import schema

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// types
+import { typeDefs } from './schema.js';
+
 // Server setup
 const server = new ApolloServer({
-  //
+  typeDefs,
+  // resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
@@ -11,3 +15,9 @@ const { url } = await startStandaloneServer(server, {
 });
 
 console.log(`Server ready at port`, 4000);
+
+/**
+ * Links for course:
+ * https://www.apollographql.com/docs/apollo-server/getting-started/
+ * https://www.youtube.com/watch?v=5199E50O7SI
+ */
